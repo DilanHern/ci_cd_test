567f925a338aa8766ff067510762506d6f1fed83
Dilan Hernandez <dilanhernandez48@gmail.com>
2025-12-10 20:03:45 -0600

    Correccion en el expected text del test

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,6 @@ const app = require("../index");
 describe("GET /", () => {
   it("should return success message", async () => {
     const res = await request(app).get("/");
-    expect(res.text).toBe("Hello from CI/CD with Vercel!");
+    expect(res.text).toBe("Hello from CI/CD Pipeline!");
   });
 });
